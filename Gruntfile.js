4d6a2834b5b55bc3cb3f5f4bbc3cfff44bdf6455
Anubhav <iamanubhavsaini@gmail.com>
2021-01-28 19:43:42 +0530

    Cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,83 +1,83 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        expand: true,
-        cwd: 'App/public/scripts/',
-        src: '*.js',
-        dest: 'Dist/public/scripts/',
-        ext: '.min.js',
-        extDot: 'last'
-      },
-    },
-
-    clean: {
-      dist: ["Dist/*"]
-    },
-
-/* I want sass to take care of SCSS and SASS files. */
-    sass: {
-      self: {
-        options: {
-          style: 'compressed',
-          sourcemap: 'none'
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                expand: true,
+                cwd: 'App/public/scripts/',
+                src: '*.js',
+                dest: 'Dist/public/scripts/',
+                ext: '.min.js',
+                extDot: 'last'
+            },
         },
-        files: [{
-          expand: true,
-          cwd: 'App/public/styles/',
-          src: ['*.scss'],
-          dest: 'Dist/public/styles/',
-          ext: '.min.css'
-        }]
-      }
-    },
 
-    haml: {
-      dist: {
-        files: {
-          'Dist/index.html': 'App/index.haml',
+        clean: {
+            dist: ["Dist/*"]
+        },
+
+        /* I want sass to take care of SCSS and SASS files. */
+        sass: {
+            self: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'App/public/styles/',
+                    src: ['*.scss'],
+                    dest: 'Dist/public/styles/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        haml: {
+            dist: {
+                files: {
+                    'Dist/index.html': 'App/index.haml',
+                }
+            }
+        },
+
+        copy: {
+            fonts: {
+                files: [{expand: true, cwd: 'App/public/fonts/', src: ['*'], dest: 'Dist/public/fonts/'}]
+            },
+            images: {
+                files: [{expand: true, cwd: 'App/public/images', src: ['*'], dest: 'Dist/public/images/'}]
+            }
+        },
+
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'App/public/styles/',
+                    src: ['*.css', '!*.min.css', '!*.scss'],
+                    dest: 'Dist/public/styles/',
+                    ext: '.min.css'
+                }]
+            }
         }
-      }
-    },
 
-    copy: {
-      fonts: {
-        files: [{expand: true, cwd: 'App/public/fonts/', src: ['*'], dest: 'Dist/public/fonts/'}]
-      },
-      images: {
-        files: [{expand: true, cwd: 'App/public/images', src: ['*'], dest: 'Dist/public/images/'}]
-      }
-    },
-
-    cssmin: {
-      target: {
-        files: [{
-          expand: true,
-          cwd: 'App/public/styles/',
-          src: ['*.css', '!*.min.css', '!*.scss'],
-          dest: 'Dist/public/styles/',
-          ext: '.min.css'
-        }]
-      }
-    }
-
-  });
+    });
 
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-haml2html');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-haml2html');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 
-  grunt.registerTask('default', ['clean:dist', 'uglify', 'sass:self', 'haml', 'copy:fonts', 'copy:images', 'cssmin']);
-/* sass:self gotta come before cssmin */
+    grunt.registerTask('default', ['clean:dist', 'uglify', 'sass:self', 'haml', 'copy:fonts', 'copy:images', 'cssmin']);
+    /* sass:self gotta come before cssmin */
 };
--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-[![Build Status](https://travis-ci.org/IAmAnubhavSaini/basic-bootstrap.svg?branch=master)](https://travis-ci.org/IAmAnubhavSaini/basic-bootstrap) <img src="https://badge.fury.io/js/basic-bootstrap.svg" /> <img src="https://david-dm.org/iamanubhavsaini/basic-bootstrap.svg" /> <img src="https://david-dm.org/iamanubhavsaini/basic-bootstrap/dev-status.svg" />
+# basic-bootstrap
 
------
+This is very basic place to start hand made application building.
 
-[![NPM](https://nodei.co/npm/basic-bootstrap.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/basic-bootstrap/)
+`npm install && grunt`
 
------
+- HAML: use for building structure.
+- JS: use for interactivity.
+- SCSS: use for advanced styling.
 
-web-dev / basic-bootstrap
----
+## LICENSE
 
-This is very basic place to start hand made application building. `npm install` it and `grunt` - it just works.
-
-HAML: use for building structure.
-JS: use for interactivity.
-SCSS: use for advanced styling.
-
-
+> MIT
